02d31055fa37e5595f1510a3bfb94d63891df020
AbdulMohaimanUmais <abdulmohaiman333@gmail.com>
2024-10-24 11:24:09 +0500

    Even and Odd Numbers Sorting

---
+++ Sort_Even_Odd.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def sortEvenOdd(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        evens = [nums[i] for i in range(len(nums)) if i%2 == 0 ]
+        odds = [nums[i] for i in range(len(nums)) if i%2 == 1 ]
+
+        evens.sort()
+        odds.sort(reverse = True)
+
+        result = []
+        even, odd = 0, 0
+
+        for i in range(len(nums)):
+            if i%2 == 0:
+                result.append(evens[even])
+                even += 1
+            else:
+                result.append(odds[odd])
+                odds += 1
+
+        return result

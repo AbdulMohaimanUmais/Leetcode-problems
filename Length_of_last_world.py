6126adba4d2bf2aaac172852402717795f1990f3
AbdulMohaimanUmais <abdulmohaiman333@gmail.com>
2024-12-09 18:24:02 +0500

    Length of lasst word.

--- Length_of_last_world.py
+++ Length_of_last_world.py
@@ -1,26 +1,18 @@
-# class Solution(object):
-#     def lengthOfLastWord(self, s):
-#         """
-#         :type s: str
-#         :rtype: int
-#         """
-#         length = 0
-#         i = len(s) - 1
+class Solution(object):
+    def lengthOfLastWord(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        length = 0
+        i = len(s) - 1
 
-#         while i >= 0 and i == " ":
-#             i -= 1
+        while i >= 0 and s[i] == " ":
+            i -= 1
 
-#         while i >= 0 and i == " ":
-#             length += 1
-#             i -= 1
+        while i >= 0 and s[i] != " ":
+            length += 1
+            i -= 1
 
-#         return length
+        return length
 
-
-
-print('Hello')
-
-i = [1,2,3,4,5]
-
-for x in i:
-    print(x)
